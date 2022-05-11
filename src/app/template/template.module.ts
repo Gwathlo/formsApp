cbc302c3dcb6df0e5dd7479144710ba9786ff77f
Gwathlo <gwath@yahoo.com>
2022-05-11 20:21:41 +0200

    Form

--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -1,21 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { TemplateRoutingModule } from './template-routing.module';
 import { BasicsComponent } from './components/basics/basics.component';
 import { DinamicsComponent } from './components/dinamics/dinamics.component';
 import { SwitchesComponent } from './components/switches/switches.component';
 
-
 @NgModule({
-  declarations: [
-    BasicsComponent,
-    DinamicsComponent,
-    SwitchesComponent
-  ],
-  imports: [
-    CommonModule,
-    TemplateRoutingModule
-  ]
+  declarations: [BasicsComponent, DinamicsComponent, SwitchesComponent],
+  imports: [CommonModule, TemplateRoutingModule, FormsModule],
 })
-export class TemplateModule { }
+export class TemplateModule {}
