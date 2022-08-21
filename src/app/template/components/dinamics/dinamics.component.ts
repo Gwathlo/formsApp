e35ad4260b95cdcb54933eabdad04262d84a5bfb
Gwathlo <gwath@yahoo.com>
2022-08-21 21:38:00 +0200

    Dinamics

--- src/app/template/components/dinamics/dinamics.component.ts
+++ src/app/template/components/dinamics/dinamics.component.ts
@@ -1,16 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+interface Person {
+  name: string;
+  favorites: Favorite[];
+}
+
+interface Favorite {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-dinamics',
   templateUrl: './dinamics.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class DinamicsComponent implements OnInit {
+export class DinamicsComponent {
+  person: Person = {
+    name: 'Alex',
+    favorites: [
+      { id: 1, name: 'Carmaggeddon' },
+      { id: 2, name: 'Monkey Island' },
+    ],
+  };
 
-  constructor() { }
-
-  ngOnInit(): void {
+  save() {
+    console.log('form saved');
   }
-
 }
