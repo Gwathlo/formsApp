5c8163477609160c5efba1cd6860f36a76f3c4c3
Gwathlo <gwath@yahoo.com>
2022-09-18 18:44:51 +0200

    Add FormBuilder and remove FormControls

--- src/app/reactive/components/basics/basics.component.ts
+++ src/app/reactive/components/basics/basics.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-basics',
@@ -7,9 +7,17 @@ import { FormGroup, FormControl } from '@angular/forms';
   styles: [],
 })
 export class BasicsComponent {
-  myForm: FormGroup = new FormGroup({
-    name: new FormControl('RTX 4080ti'),
+  // myForm: FormGroup = new FormGroup({
+  //   name: new FormControl('RTX 4080ti'),
+  //   price: new FormControl(1500),
+  //   stocks: new FormControl(5),
+  // });
+
+  myForm: FormGroup = this.fb.group({
+    name: ['RTX 4080ti'],
+    price: [1500],
+    stocks: [5],
   });
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {}
 }
