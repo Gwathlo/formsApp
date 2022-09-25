4bf6e58201e2d11e08f864174c63e3183ed66262
Gwathlo <gwath@yahoo.com>
2022-09-25 18:31:27 +0200

    Reactive form validation

--- src/app/reactive/components/dinamics/dinamics.component.ts
+++ src/app/reactive/components/dinamics/dinamics.component.ts
@@ -1,16 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamics',
   templateUrl: './dinamics.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class DinamicsComponent implements OnInit {
+export class DinamicsComponent {
+  myForm: FormGroup = this.fb.group({
+    name: [, [Validators.required, Validators.minLength(3)]],
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit(): void {
+  save() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+    this.myForm.reset();
   }
 
+  fieldIsValid(field: string) {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
 }
